5321bef290f327fbb9a4d96b11783de8f8901342
junho7108 <yyjh7108@gmail.com>
2024-06-13 16:05:05 +0900

    feat: 프로그래머스 Level2. 타겟넘버
    DFS/BFS 문제입니다.

--- Algorithm/Programmers/TargetNumber.swift
+++ Algorithm/Programmers/TargetNumber.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+// 프로그래머스 Level2. 타겟넘버
+
+struct TargetNumber: Algorithm {
+    @discardableResult
+    func solution(_ numbers:[Int], _ target:Int) -> Int {
+        var result: Int = 0
+        
+        dfs(start: 0, result: &result, currentIndex: 0, numbers: numbers, target: target)
+    
+        return result
+    }
+    
+    func dfs(start: Int, result: inout Int, currentIndex: Int, numbers: [Int], target: Int) {
+        if currentIndex == numbers.count {
+            if start == target { result += 1}
+            return
+        }
+        
+        dfs(start: start + numbers[currentIndex]
+            , result: &result,
+            currentIndex: currentIndex + 1,
+            numbers: numbers,
+            target: target)
+        
+        dfs(start: start - numbers[currentIndex]
+            , result: &result,
+            currentIndex: currentIndex + 1,
+            numbers: numbers,
+            target: target)
+    }
+  
+    func solution() {
+        self.solution([1, 1, 1, 1, 1], 3)
+        self.solution([4, 1, 2, 1], 4)
+    }
+}
