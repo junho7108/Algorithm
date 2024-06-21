e97518551f71ca6ff290dbbbd1d7c56c8b1157eb
junho7108 <yyjh7108@gmail.com>
2024-06-21 16:41:15 +0900

    feat: 백준 A->B

---
+++ Algorithm/BaekJoon/A->B.swift
@@ -0,0 +1,43 @@
+//
+//  A->B.swift
+//  Algorithm
+//
+//  Created by Junho Yoon on 6/20/24.
+//
+
+import Foundation
+
+class BaekJoon16953: Algorithm {
+    
+    func solution() {
+        
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        var result: Int = -1
+     
+        let a = input[0]
+        let b = input[1]
+      
+        func dfs(start: Int, count: Int = 0) {
+            if start == b {
+                result = count
+                return
+            }
+         
+            if start * 2 <= b {
+                dfs(start: start * 2, count: count + 1 )
+            }
+            
+            let string = "\(start)1"
+            let value = Int(string)!
+            
+            if value <= b {
+                dfs(start: value, count: count + 1)
+            }
+        }
+        
+        dfs(start: a, count: 1)
+        
+        
+        print(result)
+    }
+}
