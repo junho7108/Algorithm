198be5727586fce494ba8776f484c23c99d3e232
junho7108 <yyjh7108@gmail.com>
2024-09-07 21:07:22 +0900

    refactor: 네트워크

--- Algorithm/Programmers/네트워크.swift
+++ Algorithm/Programmers/네트워크.swift
@@ -11,40 +11,30 @@ class 네트워크: Algorithm {
     
     @discardableResult
     private func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-        var netWork: [Int: [Int]] = [:]
-        var startNumberList: Set<Int> = Set<Int>()
-        var visited: [Bool] = [Bool](repeating: false, count: n + 1)
+        var startNumberList: Set<Int> = []
+        var visited: [Bool] = [Bool](repeating: false, count: computers.count)
         
-        for i in 1 ... n { netWork.updateValue([], forKey: i) }
-    
-        computers.enumerated().forEach { (index, computer) in
-            for i in 0 ..< computer.count {
-                if i != index && computer[i] == 1 {
-                    netWork[index + 1]?.append(i + 1)
-                }
-            }
-        }
-        
-        func bfs() {
-            for i in 1 ... n {
-                var queue: [Int] = [i]
-             
-                while !queue.isEmpty {
-                    let first = queue.removeFirst()
+        func bfs(start: Int) {
+            var queue: [Int] = [start]
+            
+            while !queue.isEmpty {
+                let start = queue.removeFirst()
                 
-                    if !visited[first] {
-                        visited[first] = true
-                        startNumberList.insert(i)
-                      
-                        if let list = netWork[first] {
-                            list.forEach { queue.append($0) }
-                        }
+                if !visited[start] {
+                    visited[start] = true
+                    
+                    for (index, elem) in computers[start].enumerated() {
+                        if elem == 1 { queue.append(index) }
                     }
                 }
             }
         }
-     
-        bfs()
+        
+        for i in 0 ..< computers.count {
+            if !visited[i] { startNumberList.insert(i) }
+            bfs(start: i)
+        }
+        
         return startNumberList.count
     }
     
